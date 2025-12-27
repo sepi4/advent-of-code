a6927d612d3e51000764bb3e8f6a516740a1c1b5
Sergei Polishsuk <polser1@gmail.com>
2025-12-27 03:04:09 +0200

    04 part2

--- 2025/04/main.go
+++ 2025/04/main.go
@@ -54,6 +54,47 @@ func part1() {
 	fmt.Println("sum:", sum)
 }
 
+func collectToBeRemoved(rows []string) [][]int {
+	limit := 4
+	arr := make([][]int, 0)
+	for y := 0; y < len(rows); y++ {
+		for x := 0; x < len(rows[0]); x++ {
+			if rows[y][x] == '@' && getNextAmount(rows, y, x) < limit {
+				arr = append(arr, []int{y, x})
+			}
+		}
+	}
+	return arr
+}
+
+func makeRemove(rows []string, arr [][]int) []string {
+	for _, v := range arr {
+		y, x := v[0], v[1]
+		rr := []rune(rows[y])
+		rr[x] = '.'
+		rows[y] = string(rr)
+	}
+	return rows
+}
+
+func part2() {
+	// s := lib.ReadInput("04/example.txt")
+	s := lib.ReadInput("04/input.txt")
+	sum := 0
+	rows := lib.GetRows(s)
+	toRemove := collectToBeRemoved(rows)
+	// fmt.Println(rows)
+	for len(toRemove) > 0 {
+		// make remove
+		sum += len(toRemove)
+		rows = makeRemove(rows, toRemove)
+		toRemove = collectToBeRemoved(rows)
+	}
+	// fmt.Println(rows)
+	fmt.Println(sum)
+}
+
 func main() {
-	part1()
+	// part1()
+	part2()
 }
