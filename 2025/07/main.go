577850e8556653272d37b7417969e393535ae6dd
Sergei Polishsuk <polser1@gmail.com>
2026-02-04 04:54:11 +0200

    07 in go

--- 2025/07/main.go
+++ 2025/07/main.go
@@ -2,123 +2,83 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"strings"
 
 	"2025/lib"
 )
 
-type Stream struct {
-	y          int
-	x          int
-	splitCount int
-}
-
 const SPLITTER = '^'
 
-func checkSamePosition(arr []*Stream, y int, x int) []*Stream {
-	aa := []*Stream{}
-	for _, v := range arr {
-		if v.y == y && v.x == x {
-			aa = append(aa, v)
-		}
-	}
-	newaa := []*Stream{}
-	for _, v := range arr {
-		if !slices.Contains(aa, v) {
-			newaa = append(newaa, v)
-		}
-	}
-
-	if len(aa) == 2 {
-		newaa = append(newaa, &Stream{
-			y:          aa[0].y,
-			x:          aa[0].x,
-			splitCount: aa[0].splitCount + aa[1].splitCount,
-		})
-		return newaa
-	}
-	return arr
+type Stream struct {
+	y       int
+	x       int
+	counter int
 }
 
-func part1() {
-	s := lib.ReadInput("07/example.txt")
-	// s := lib.ReadInput("07/input.txt")
-	rr := lib.GetRows(s)
-	rows := [][]rune{}
-	for _, r := range rr {
-		rows = append(rows, []rune(r))
-	}
+func (s *Stream) down() {
+	s.y++
+}
 
-	// for _, r := range rows {
-	// 	fmt.Println(r)
-	// }
+func (s *Stream) left() {
+	s.x--
+}
 
-	// FIND START
-	startIndex := -1
-	for i, x := range rows[0] {
-		if x == 'S' {
-			startIndex = i
-			break
+func (s *Stream) right() {
+	s.x++
+}
+
+func (s *Stream) updateCounter(num int) {
+	s.counter = num
+}
+
+func removeDups(ss []*Stream) []*Stream {
+	for i := 0; i < len(ss)-1; i++ {
+		for j := i + 1; j < len(ss); j++ {
+			a := ss[i]
+			b := ss[j]
+			if a.y == b.y && a.x == b.x {
+				a.updateCounter(a.counter + b.counter)
+				ss = append(ss[:j], ss[j+1:]...)
+				j--
+			}
 		}
 	}
+	return ss
+}
 
-	ss := []*Stream{{
-		y:          1,
-		x:          startIndex,
-		splitCount: 0,
-	}}
-
-	moved := true
-	for moved {
-		moved = false
-		// MOVE STREAMS
-		for i := 0; i < len(ss); i++ {
-			if rows[ss[i].y][ss[i].x] != SPLITTER {
-				rows[ss[i].y][ss[i].x] = '|'
-			}
-			if ss[i].y == len(rows)-1 {
-				continue
-			}
-			// move down current
-			ss[i].y++
-			moved = true
-			// add another to right
-			if rows[ss[i].y][ss[i].x] == SPLITTER {
-				s2 := &Stream{
-					y:          ss[i].y,
-					x:          ss[i].x + 1,
-					splitCount: 0,
-				}
-				ss = append(ss, s2)
-				ss = checkSamePosition(ss, ss[i].y, ss[i].x)
-				ss[i].x-- // move current to left
-				ss[i].splitCount++
-				// if len(aaa) > 1 {
-				// 	fmt.Println("aaa", aaa)
-				// 	return
-				// }
-			}
-			ss = checkSamePosition(ss, ss[i].y, ss[i].x)
-		}
+func solve() {
+	// text := lib.ReadInput("07/example.txt")
+	text := lib.ReadInput("07/input.txt")
+	rows := lib.GetRows(text)
+	splitsAmount := 0
+	startIndex := strings.Index(rows[0], "S")
+	ss := []*Stream{ // need * because we want to array of pointers
+		{0, startIndex, 1},
 	}
 
-	// count splits
-	count := 0
+	for ss[0].y != len(rows)-1 {
+		newSs := []*Stream{}
+		for _, s := range ss {
+			s.down()
+			if rows[s.y][s.x] == SPLITTER {
+				splitsAmount++
+				s2 := &Stream{s.y, s.x, s.counter}
+				s.left()
+				s2.right()
+				newSs = append(newSs, s2)
+			}
+		}
+		ss = append(ss, newSs...)
+		ss = removeDups(ss)
+	}
+	sum := 0
 	for _, s := range ss {
-		count += s.splitCount
+		sum += s.counter
 	}
-
-	// for _, r := range rows {
-	// 	fmt.Println(string(r))
-	// }
-	fmt.Println("ss:", ss)
-	fmt.Println("count:", count)
-}
-
-func part2() {
+	fmt.Println("splits:", splitsAmount)
+	fmt.Println("sum:", sum)
 }
 
 func main() {
-	part1()
-	// part2()
+	solve()
 }
