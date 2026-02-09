284b2cdc908f1268dfe32faff41fe0a816d7b10e
Sergei Polishsuk <polser1@gmail.com>
2026-02-09 21:41:36 +0200

    07-2 with rust

--- 2025/07/rust/src/main.rs
+++ 2025/07/rust/src/main.rs
@@ -5,17 +5,13 @@ fn read_input(file_path: &str) -> String {
     text
 }
 
-
 #[derive(Debug)]
 struct Stream {
     coor: (usize, usize), // (y, x)
-}
-
-fn part2() {
+    counter: usize,
 }
 
 fn remove_same_position(ss: &mut Vec<Stream>) {
-    // println!("removing {:?}",ss);
     let mut i = 0;
     while i < ss.len() - 1 {
         let mut j = i + 1;
@@ -23,31 +19,34 @@ fn remove_same_position(ss: &mut Vec<Stream>) {
             let (ay, ax) = ss[i].coor;
             let (by, bx) = ss[j].coor;
             if ay == by && ax == bx {
+                ss[i].counter = ss[i].counter + ss[j].counter;
                 ss.remove(j);
             }
             j += 1;
         }
         i += 1;
     }
-    // println!("after removing {:?}",ss);
 }
 
-fn part1(cells: &Vec<Vec<char>>) {
+fn part1_and_part2(cells: &Vec<Vec<char>>) {
     let mut count_splits = 0;
-    // println!("{:?}", );
     let si = cells[0].iter().position(|c| *c == 'S');
     if si == None {
         panic!("not found starting 'S'");
     }
-    let mut streams: Vec<Stream> = vec![Stream{coor: (0, si.unwrap())}];
+    let mut streams: Vec<Stream> = vec![Stream{coor: (0, si.unwrap()), counter: 1}];
     loop {
         let mut next_row: Vec<Stream> = vec![];
         for s in &mut streams {
-
             s.coor.0 += 1;
 
             if cells[s.coor.0][s.coor.1] == '^' {
-                next_row.push(Stream{coor: (s.coor.0, s.coor.1 + 1)});
+                next_row.push(
+                    Stream{
+                        coor: (s.coor.0, s.coor.1 + 1), 
+                        counter: s.counter
+                    }
+                );
                 s.coor.1 -= 1;
                 count_splits += 1;
             }
@@ -62,31 +61,21 @@ fn part1(cells: &Vec<Vec<char>>) {
             break;
         }
     }
-    println!("{:?}", streams.len());
-    println!("splits {:?}", count_splits);
+    println!("part1 ================================");
+    println!("splits: {:?}", count_splits);
+    println!("part2 ================================");
+    println!("possible routes: {:?}", streams.iter().map(|x| x.counter).sum::<usize>());
 }
 
 
 fn main() {
-    let text = read_input("../example.txt");
-    // let text = read_input("../input.txt");
+    // let text = read_input("../example.txt");
+    let text = read_input("../input.txt");
 
-    // println!("{:?}", text);
     let cells = text.split('\n')
         .filter(|row| !row.is_empty())
         .map(|row| row.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
     
-    // // println!("{:?}", cells);
-    // for row in cells {
-    //     println!("{:?}", row);
-    // }
-
-    println!("part1 ================================");
-    part1(&cells);
-    
-    // println!("part2 ================================");
-    // part2(&rows);
-    
-
+    part1_and_part2(&cells);
 }
